bdf7a2a2679eebdb22347792f4096a91cf160cb8
hasrat ali <habvirgo12@gmail.com>
2026-02-02 12:09:31 +0300

    Update questions.py

--- config/questions.py
+++ config/questions.py
@@ -33,7 +33,7 @@ require_visa = "No"               # "Yes" or "No"
 website = "https://github.com/GodsScion"                        # "www.example.bio" or "" and so on....
 
 # Please provide the link to your LinkedIn profile.
-linkedIn = "https://www.linkedin.com/in/saivigneshgolla/"       # "https://www.linkedin.com/in/example" or "" and so on...
+linkedIn = "www.linkedin.com/in/hasrat-ali-bangash512"       # "https://www.linkedin.com/in/example" or "" and so on...
 
 # What is the status of your citizenship? # If left empty as "", tool will not answer the question. However, note that some companies make it compulsory to be answered
 # Valid options are: "U.S. Citizen/Permanent Resident", "Non-citizen allowed to work for any employer", "Non-citizen allowed to work for current employer", "Non-citizen seeking work authorization", "Canadian Citizen/Permanent Resident" or "Other"
@@ -44,7 +44,7 @@ us_citizenship = "U.S. Citizen/Permanent Resident"
 ## SOME ANNOYING QUESTIONS BY COMPANIES 🫠 ##
 
 # What to enter in your desired salary question (American and European), What is your expected CTC (South Asian and others)?, only enter in numbers as some companies only allow numbers,
-desired_salary = 1200000          # 80000, 90000, 100000 or 120000 and so on... Do NOT use quotes
+desired_salary = QAR 9000          # 80000, 90000, 100000 or 120000 and so on... Do NOT use quotes
 '''
 Note: If question has the word "lakhs" in it (Example: What is your expected CTC in lakhs), 
 then it will add '.' before last 5 digits and answer. Examples: 
@@ -56,7 +56,7 @@ And if asked in months, then it will divide by 12 and answer. Examples:
 '''
 
 # What is your current CTC? Some companies make it compulsory to be answered in numbers...
-current_ctc = 800000            # 800000, 900000, 1000000 or 1200000 and so on... Do NOT use quotes
+current_ctc = QAR 7500            # 800000, 900000, 1000000 or 1200000 and so on... Do NOT use quotes
 '''
 Note: If question has the word "lakhs" in it (Example: What is your current CTC in lakhs), 
 then it will add '.' before last 5 digits and answer. Examples: 
@@ -97,7 +97,14 @@ Note: If left empty as "", the tool will not answer the question. However, note 
 ''' 
 
 # Your cover letter in quotes, use \n to add line breaks if using single quotes "Cover Letter".You can skip \n if using triple quotes """Cover Letter""" (This question makes sense though)
-cover_letter = """
+cover_letter = "Dear Hiring Manager,
+I am writing to express my interest in an IT position where I can contribute my technical expertise, customer-focused approach, and problem-solving skills. With over 10 years of experience in IT support and service-oriented environments, I have developed a strong ability to support users, maintain systems, and ensure reliable IT operations.
+My experience includes providing end-user and enterprise-level support across multiple platforms, managing incidents and service requests in line with SLAs and KPIs, and working with technologies such as Windows Server, Active Directory, Microsoft 365, Azure Cloud,  Intune, and ITSM tools. I am comfortable troubleshooting hardware, software, and network-related issues while maintaining a professional and calm communication style.
+In addition to technical skills, I bring strong customer service experience, enabling me to translate technical issues into clear, user-friendly solutions. I have worked in fast-paced environments, collaborated with cross-functional teams, and consistently maintained high service quality standards.
+am highly motivated, adaptable, and eager to continue learning new technologies. I am confident that my combination of technical knowledge, reliability, and customer-first mindset would allow me to add value to any IT team.
+Thank you for your time and consideration. I look forward to the opportunity to further discuss how my skills and experience align with your requirements.
+Yours sincerely,
+Hasrat Ali"
 Cover Letter
 """
 ##> ------ Dheeraj Deshwal : dheeraj9811 Email:dheeraj20194@iiitd.ac.in/dheerajdeshwal9811@gmail.com - Feature ------
@@ -113,7 +120,7 @@ Note: If left empty as "", the tool will not answer the question. However, note 
 ''' 
 
 # Name of your most recent employer
-recent_employer = "Not Applicable" # "", "Lala Company", "Google", "Snowflake", "Databricks"
+recent_employer = "Cyber Jeck" # "", "Lala Company", "Google", "Snowflake", "Databricks"
 
 # Example question: "On a scale of 1-10 how much experience do you have building web or mobile applications? 1 being very little or only in school, 10 being that you have built and launched applications to real users"
 confidence_level = "8"             # Any number between "1" to "10" including 1 and 10, put it in quotes ""
